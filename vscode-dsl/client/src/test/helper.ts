cdecb6e41c2492eb31dca860c796da8fb60146e6
jinjor <jinjorweb@gmail.com>
2020-03-28 15:30:59 +0900

    rename extension

--- vscode-dsl/client/src/test/helper.ts
+++ vscode-dsl/client/src/test/helper.ts
@@ -16,7 +16,7 @@ export let platformEol: string;
  */
 export async function activate(docUri: vscode.Uri) {
   // The extensionId is `publisher.name` from package.json
-  const ext = vscode.extensions.getExtension("jinjor.dsl-ls")!;
+  const ext = vscode.extensions.getExtension("jinjor.vscode-dsl")!;
   await ext.activate();
   try {
     doc = await vscode.workspace.openTextDocument(docUri);
