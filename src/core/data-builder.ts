52c2ad19c11c39bf10f7b9a1dd30af9342eeff43
jinjor <jinjorweb@gmail.com>
2020-04-03 01:09:16 +0900

    refactor builder

--- src/core/data-builder.ts
+++ src/core/data-builder.ts
@@ -1,24 +1,51 @@
+import * as assert from "assert";
+
 export class DataBuilder {
   public size = 0;
-  private data: Uint8Array[] = [];
+  private data: (Uint8Array | Int32Array | Float32Array)[] = [];
   constructor() {}
-  push(data: Uint8Array): number {
+  push(data: Uint8Array | Int32Array | Float32Array): number {
     this.data.push(data);
     const offset = this.size;
     this.size += data.byteLength;
     return offset;
   }
-  pushByte(byte: number): number {
-    return this.push(new Uint8Array([byte]));
+  pushUint8(n: number): number {
+    return this.push(new Uint8Array([n]));
+  }
+  pushInt32(n: number): number {
+    return this.push(new Int32Array([n]));
+  }
+  pushFloat32(n: number): number {
+    return this.push(new Float32Array([n]));
   }
   createData(): Uint8Array {
+    const buffer = new ArrayBuffer(this.size);
+    const view = new DataView(buffer);
     let offset = 0;
-    const data = new Uint8Array(this.size);
     for (let i = 0; i < this.data.length; i++) {
-      data.set(this.data[i], offset);
+      let offsetBranch = offset;
+      const d = this.data[i];
+      if (d instanceof Uint8Array) {
+        for (const v of d.values()) {
+          view.setUint8(offsetBranch, v);
+          offsetBranch++;
+        }
+      } else if (d instanceof Int32Array) {
+        for (const v of d.values()) {
+          view.setInt32(offsetBranch, v);
+          offsetBranch += 4;
+        }
+      } else if (d instanceof Float32Array) {
+        for (const v of d.values()) {
+          view.setFloat32(offsetBranch, v);
+          offsetBranch += 4;
+        }
+      }
       offset += this.data[i].byteLength;
+      assert.equal(offset, offsetBranch);
     }
-    return data;
+    return new Uint8Array(buffer);
   }
 }
 
@@ -36,7 +63,7 @@ export class StringBuilder {
     }
     const stringData = encode(value);
     // first byte is length
-    const offset = this.dataBuilder.pushByte(stringData.byteLength);
+    const offset = this.dataBuilder.pushUint8(stringData.byteLength);
     this.dataBuilder.push(stringData);
     this.offsetMap.set(value, offset);
   }
